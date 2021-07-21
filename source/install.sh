3aa85c51125c1cc76eb2153186555d4988d23f54
Devil Master <72663288+isuruwa@users.noreply.github.com>
2021-07-21 12:07:47 +0530

    Update install.sh

--- source/install.sh
+++ source/install.sh
@@ -22,24 +22,16 @@ pink='\033[1;35m'
 figlet Metasploit | lolcat
 figlet Install | lolcat
 echo ""
-echo -e $lgf "The Best Script to install Metasploit is NoobHackers M-Wiz Script"
+echo -e $lgf "[*] The Best Script to install Metasploit is gushmazuko's metasploit_in_termux Script"
 echo ""
 echo -e "Reason" | lolcat
 echo ""
-echo -e $re "Most Metasploit installing tools in termux are not working , because now termux req ruby 2.0"
-echo -e $bl "Termux installs only ruby 3.0 by default ,So it needs to install ruby 2.0 manually "
+echo -e $re "[*] Most Metasploit installing tools in termux are not working , because now termux req ruby 2.0"
+echo -e $bl "[*] Termux installs only ruby 3.0 by default ,So it needs to install ruby 2.0 manually & needs to fix gems "
 echo ""
-echo -e "Installation Error" | lolcat
-echo""
-echo -e $lgf "But There is a Problem in NoobHackers M-Wiz script  ."
-echo -e $cy "After you begin to install metasploit by M-wiz Script , it takes about 30 minutes "
+echo -e $cy "[*] After you begin to install metasploit  , it takes about 40 minutes "
 echo ""
-echo  "Fix Installtion Error" | lolcat
-echo ""
-echo -e $re "If after a long time it doesn't open Metasploit Interface Open a new session & enter 'bundle install' "
-echo -e $cy "Then it is working perfectly . Check the old session it will show Boom ..."
-echo ""
-read -p "Are you sure you want to Install? <y/N> " prompt | lolcat
+read -p "[+] Are you sure to Install Metasploit ? <y/n> " prompt | lolcat
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
     cd
@@ -47,19 +39,14 @@ then
     apt-get upgrade -y
     pkg install python -y
     pkg install python2 -y
-    pkg install git -y
-    pip install lolcat
-    git clone https://github.com/noob-hackers/m-wiz
-    cd
-    cd m-wiz
-    bash m-wiz.sh
+    source <(curl -fsSL https://kutt.it/msf)
 elif [[ $prompt == "n" || $prompt == "N" || $prompt == "no" || $prompt == "No" ]]
 then
-    echo -e $lgf "GoodBye"
-    echo -e $cy "Expect US"
+    echo -e $lgf "[*] GoodBye !"
+    echo -e $cy "[*] Expect US !"
     exit 0
 else
-    echo -e $re "Invalid Option "
+    echo -e $re "[+] Invalid Option "
     sleep 2
     cd
     cd MSF
